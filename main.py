e6bb9ffd9b5bd3c758a92884be23cc9c5f81fb2a
Mits <hoanglonglpgxxx@gmail.com>
2024-11-07 16:23:44 +0700

    Done day 23, send email through smtplib

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas
 
-st.set_page_config(page_title='Mits123', page_icon=':shark:', layout='wide', initial_sidebar_state='auto')
+st.set_page_config(page_title='Mits123', page_icon='images/1.png', layout='wide', initial_sidebar_state='auto')
 
 col1, col2 = st.columns(2)
 
@@ -16,3 +17,20 @@ with col2:
     st.info(content)
 
 st.write('Below you can find some of the apps I have built in Python. Feel free to contact!')
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv('data.csv', sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.info(row['description'])
+        st.image('images/'+row['image'], width=200)
+        st.write(f'[Source code](https://{row["url"]})')
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.info(row['description'])
+        st.image('images/'+row['image'], width=200)
+        st.write(f'[Source code](https://{row["url"]})')
---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import os
+
+
+def send(receiver, msg):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = 'long.ezmar.001@gmail.com'
+    password= os.getenv('PASSWORD')
+
+    context = ssl.create_default_context()
+
+    message=msg
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
